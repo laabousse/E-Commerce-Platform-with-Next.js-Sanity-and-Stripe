1fb475cfe8dba0bd85a1724f1dfacbd44951e96a
laabousse <idriss.laabousse@gmail.com>
2024-12-03 02:07:36 +0800

    fix bugs

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -3,7 +3,7 @@ import type { StructureResolver } from "sanity/structure";
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title("Shopr ecommerce website")
+    .title("Trendy e-commerce website")
     .items([
       S.documentTypeListItem("category").title("Categories"),
       S.divider(),
